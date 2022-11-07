33b17dfcc7dde64e75a9b47cf757a6df74d9d002
Brandon Knight <knightbrandon1984@gmail.com>
2022-11-07 11:57:39 -0600

    all the folders

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,20 @@ CREATE plants_db;
 
 USE plants_db;
 
-CREATE TABLE 8b (
+CREATE TABLE main_plant (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     plant_name VARCHAR(50) NOT NULL, 
-    when_to_plant VARCHAR(50)
-)
+    when_to_plant VARCHAR(50),
+    planting_zone VARCHAR(25)
+);
+
+CREATE TABLE companion (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    helper_plant VARCHAR(50) NOT NULL,
+    helps VARCHAR(50) NOT NULL, 
+    main_plant VARCHAR(50) NOT NULL,
+    plant_type VARCHAR(50) NOT NULL,
+    FOREIGN KEY (main_plant)
+    REFERENCES 8b(plant_name)
+    ON DELETE SET NULL
+);
