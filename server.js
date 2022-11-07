5daa3debec7c22c9a62a18ef6c7810945f72ca40
David Garcia <parradavid300@yahoo.com>
2022-11-07 10:24:54 -0600

    switched the sequelize.sync and console.log lines 37 and 38

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
 app.use(session(sess));
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+    sequelize.sync({ force: false });
+    console.log(`App listening on port ${PORT}!`);
+
 });
