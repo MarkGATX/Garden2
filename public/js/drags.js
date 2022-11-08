0812aefbbf469f0e9bdcedb6b6cd6d3726367790
Mark Gardner <themarkgardner@gmail.com>
2022-11-08 15:06:19 -0600

    added drag and drop functions to plants. Added button to clearn garden of plants. Added button for icebox feature to save garden

---
+++ public/js/drags.js
@@ -0,0 +1,38 @@
+// $( function() {
+//       $( ".gardenPlots" ).draggable();
+//     } );
+
+   $ ( function() {
+      $( ".plant" ).draggable( {opacity: 0.7, helper:'clone',  snap:'.gardenPlots'} );
+      $( ".gardenPlots" ).draggable( {opacity: 0.7, helper:'clone',  snap:'.gardenPlots'} );
+      $( ".gardenPlots" ).droppable(
+            {
+            accept: '.plant',
+            tolerance: 'fit',
+            drop: function (event, ui) {
+                  var droppable = $(this);
+                  var draggable = ui.draggable;
+                  // Move draggable into droppable
+                  draggable.clone().appendTo(droppable); 
+              }
+      });
+      });
+
+
+const clearGarden = async (event) => {
+      event.preventDefault();
+     const plots = document.querySelectorAll('.gardenPlots');
+     for (i=0; i < plots.length; i++) {
+      console.log(plots[i])
+      plots[i].innerHTML = ''
+     }
+}
+
+const saveGarden = async (event) => {
+      event.preventDefault();
+
+}
+
+document.querySelector('.clearGarden').addEventListener('click', clearGarden);
+
+  document.querySelector('.saveGarden').addEventListener('click', saveGarden);
