af8e791b65bc2e000f7ed815b087882c50314278
David Garcia <parradavid300@yahoo.com>
2022-11-09 13:23:04 -0600

    fixed server.js, fixed main handlebars, fixed paths

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,5 @@
 DROP DATABASE IF EXISTS plants_db;
-CREATE plants_db;
+CREATE DATABASE plants_db;
 
 USE plants_db;
 
-CREATE TABLE main_plant (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    plant_name VARCHAR(50) NOT NULL, 
-    when_to_plant VARCHAR(50),
-    planting_zone VARCHAR(25)
-);
-
-CREATE TABLE companion (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    helper_plant VARCHAR(50) NOT NULL,
-    helps VARCHAR(50) NOT NULL, 
-    main_plant VARCHAR(50) NOT NULL,
-    plant_type VARCHAR(50) NOT NULL,
-    FOREIGN KEY (main_plant)
-    REFERENCES 8b(plant_name)
-    ON DELETE SET NULL
-);
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const drake = require('dragula');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -32,9 +32,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
